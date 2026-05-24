1635b492810bc67e2e1f0137272d4c5ed977748d
Ryuya Asada <ryuya0v0mac@gmail.com>
2026-05-24 13:00:52 +0900

    console等が、eslintのno-undefに検知される問題に対処

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,24 @@
 import { defineConfig } from 'eslint/config';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
   {
     files: ['**/*.{js,mjs}'],
+    languageOptions: {
+      globals: globals.node,
+    },
     rules: {
       'no-unused-vars': 'error',
+      'no-undef': 'error',
     },
   },
   ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts}'],
+    languageOptions: {
+      globals: globals.node,
+    },
     rules: {
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'error',
