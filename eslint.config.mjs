ed68f2225a85fd6335be987aebda1d0dcc35728a
Ryuya Asada <ryuya0v0mac@gmail.com>
2026-05-24 13:13:19 +0900

    インポートするライブラリの順番を保存時に並び替えるように変更

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'eslint/config';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -8,9 +9,14 @@ export default defineConfig(
     languageOptions: {
       globals: globals.node,
     },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
     rules: {
       'no-unused-vars': 'error',
       'no-undef': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
   },
   ...tseslint.configs.recommended,
@@ -19,9 +25,14 @@ export default defineConfig(
     languageOptions: {
       globals: globals.node,
     },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
     rules: {
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
   },
 );
